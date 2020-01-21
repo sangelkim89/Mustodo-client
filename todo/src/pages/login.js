a0eaf037301ba1b82f02d37ecfd9938e1ac15715
sangelkim89 <sangelkim89@gmail.com>
2020-01-21 17:01:43 +0900

    to pull from upstream

--- todo/src/pages/login.js
+++ todo/src/pages/login.js
@@ -1,13 +1,64 @@
-import React from 'react'
-
-
-
+import React from "react";
+import { Link, withRouter, Redirect } from "react-router-dom";
+import axios from "axios";
+axios.defaults.withCredentials = true;
 class Login extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange = key => e => {
+    this.setState({
+      [key]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    console.log("this.props is: ", this.props);
+    e.preventDefault();
+    const { email, password } = this.state;
+    const { handleIsLoginChange } = this.props;
+    // this.props.history.push("todopage");
+    axios
+      .post("http://localhost:4000/user/login", {
+        email: email,
+        password: password
+      })
+      .then(() => {
+        handleIsLoginChange();
+        this.props.history.push("/todopage");
+        // return <Redirect to="/todopage" />;
+      })
+      .catch(err => console.log(err));
+  };
+  render() {
     return (
-      <p>Login</p>
-    )
+      <>
+        <h1>Sign In</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder={"이메일"}
+            value={this.state.email}
+            onChange={this.handleChange("email")}
+          />
+          <div>
+            <input
+              placeholder={"비밀번호"}
+              value={this.state.password}
+              onChange={this.handleChange("password")}
+            />
+          </div>
+          <div>
+            <Link to="/signup">아직 아이디가 없으신가요?</Link>
+          </div>
+          <button type="submit">로그인</button>
+        </form>
+      </>
+    );
   }
 }
-
-export default Login;
+export default withRouter(Login);
