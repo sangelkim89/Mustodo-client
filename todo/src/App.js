036bc80025a873d58efbc9b3310248190c7cab6b
sangelkim89 <sangelkim89@gmail.com>
2020-01-21 15:40:34 +0900

    signup, mypage, app

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,37 +1,63 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import axios from "axios";
+import Homepage from "../src/pages/homepage";
+import Login from "../src/pages/login";
+import Mypage from "../src/pages/mypage";
+import Signup from "../src/pages/signup";
+import Todopage from "../src/pages/todopage";
 
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-
-import Homepage from '../src/pages/homepage'
-import Login from '../src/pages/login'
-import Mypage from '../src/pages/mypage'
-import Signup from '../src/pages/signup'
-import Todopage from '../src/pages/todopage'
-
-function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1> 
-      <Router>
-        <Switch>
-          
-          <Route path="/" exact component={Homepage}/>
-          <Route path="/login" exact component={Login}/>
-          <Route path="/mypage" exact component={Mypage}/>
-          <Route path="/signup" exact component={Signup}/>
-          <Route path="/todopage" exact component={Todopage}/>
-          <Redirect from="*" to="/">
-          </Redirect>
-        </Switch>
-      </Router>
-
-    </div>
-
-    
-
-  );
+axios.defaults.withCredentials = true;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogin: false,
+      userinfo: {},
+      DBtodos: ""
+    };
+    this.handleIsLoginChange = this.handleIsLoginChange.bind(this);
+  }
+  handleIsLoginChange = () => {
+    // this.setState({ isLogin: true });
+    const savedTodoData = axios.get("http://localhost:4000/user/todopage");
+    this.setState({
+      isLogin: true,
+      DBtodos: savedTodoData
+    });
+  };
+  render() {
+    const { isLogin, userinfo, DBtodos } = this.state;
+    return (
+      <>
+        <h1>MusToDo</h1>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Homepage} />
+            <Route
+              path="/login"
+              render={() => (
+                <Login handleIsLoginChange={this.handleIsLoginChange} />
+              )}
+            />
+            <Route
+              path="/mypage"
+              exact
+              component={Mypage}
+              userInfo={userinfo}
+            />
+            <Route path="/signup" exact component={Signup} />
+            <Route path="/todopage" exact component={Todopage} />
+            <Redirect from="*" to="/"></Redirect>
+          </Switch>
+        </Router>
+      </>
+    );
+  }
 }
-
-
-
 export default App;
