ec72fe093bf5c0d2238f698f7504f6edf565d5ab
nohsikwan <tlrhks2869@naver.com>
2020-01-21 16:02:27 +0900

    todoPage_LoginPage

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,37 +1,44 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import axios from 'axios';
 
-import Homepage from '../src/pages/homepage'
-import Login from '../src/pages/login'
-import Mypage from '../src/pages/mypage'
-import Signup from '../src/pages/signup'
-import Todopage from '../src/pages/todopage'
+import Homepage from '../src/pages/homepage';
+import Login from '../src/pages/login';
+import Mypage from '../src/pages/mypage';
+import Signup from '../src/pages/signup';
+import Todopage from '../src/pages/todopage';
+import Timer from '../src/pages/timer';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1> 
-      <Router>
-        <Switch>
-          
-          <Route path="/" exact component={Homepage}/>
-          <Route path="/login" exact component={Login}/>
-          <Route path="/mypage" exact component={Mypage}/>
-          <Route path="/signup" exact component={Signup}/>
-          <Route path="/todopage" exact component={Todopage}/>
-          <Redirect from="*" to="/">
-          </Redirect>
-        </Switch>
-      </Router>
+axios.defaults.withCredentials = true;
 
-    </div>
+class App extends React.Component {
+	state = {
+		isLogin: false
+	};
+	handleIsLoginChange = () => {
+		this.setState({ isLogin: true });
+	};
 
-    
+	render() {
+		const { isLogin } = this.state;
+		return (
+			<>
+				<Timer />
+				<Router>
+					<Switch>
+						<Route exact path="/" render={() => <Homepage isLogin={isLogin} />} />
+						<Route path="/login" render={() => <Login handleIsLoginChange={this.handleIsLoginChange} />} />
+						<Route exact path="/signup" render={() => <Signup isLogin={isLogin} />} />
+						<Route exact path="/mypage" render={() => <Mypage isLogin={isLogin} />} />
+						<Route exact path="/todopage" render={() => <Todopage />} />
 
-  );
+						{/* {!isLogin ? <Redirect from="*" to="/login" /> : <Redirect from="*" to="/" />} */}
+					</Switch>
+				</Router>
+			</>
+		);
+	}
 }
-
-
 
 export default App;
--- todo/src/pages/login.js
+++ todo/src/pages/login.js
@@ -1,13 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import axios from 'axios';
 
-
+axios.defaults.withCredentials = true;
 
 class Login extends React.Component {
-  render () {
-    return (
-      <p>Login</p>
-    )
-  }
+	state = {
+		email: '',
+		password: ''
+	};
+	handleChange = key => e => {
+		this.setState({
+			[key]: e.target.value
+		});
+	};
+	handleSubmit = e => {
+		e.preventDefault();
+		const { email, password } = this.state;
+		const { handleIsLoginChange } = this.props;
+		
+		return axios
+			.post('http://localhost:4000/user/login', {
+				email: email,
+				password: password
+			})
+			.then(() => {
+				handleIsLoginChange();
+				this.props.history.push('/');
+			})
+			.catch(err => console.log(err));
+	};
+
+	render() {
+		return (
+			<>
+				<h1>Sign In</h1>
+				<form onSubmit={this.handleSubmit}>
+					<input placeholder={'이메일'} value={this.state.email} onChange={this.handleChange('email')} />
+					<div>
+						<input
+							placeholder={'비밀번호'}
+							value={this.state.password}
+							onChange={this.handleChange('password')}
+						/>
+					</div>
+					<div>
+						<Link to="/signup">아직 아이디가 없으신가요?</Link>
+					</div>
+					<button type="submit">로그인</button>
+				</form>
+			</>
+		);
+	}
 }
 
-export default Login;
+export default withRouter(Login);
