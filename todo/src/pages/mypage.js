7517ef2dc63f536b1d62f83a8f13d2e0e21ab2eb
Jun shik Ohm <zzunsic0612@gmail.com>
2020-01-22 10:55:16 +0900

    fixed mypage

--- todo/src/pages/mypage.js
+++ todo/src/pages/mypage.js
@@ -3,7 +3,7 @@ import { withRouter, Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import "./mypage.css";
 import DonutChart from "react-donut-chart";
-import Calendar from "react-calendar";
+//import Calendar from "react-calendar";
 
 axios.defaults.withCredentials = true;
 
@@ -13,6 +13,7 @@ axios.defaults.withCredentials = true;
 //   todoCount: int,
 //   completeCount: int
 // }
+
 class Mypage extends React.Component {
   //dont forget to rupdate states with props!
   constructor(props) {
@@ -22,9 +23,9 @@ class Mypage extends React.Component {
       userinfo: {},
       todoCount: 89,
       completeCount: 26,
-      username: "placeholder username",
-      email: "placeholder email",
-      password: "placeholder password",
+      username: "username",
+      email: "email",
+      password: "password",
       date: new Date()
     };
     this.handleInputValue = this.handleInputValue.bind(this);
@@ -36,6 +37,7 @@ class Mypage extends React.Component {
     this.emailEditButtonRef = React.createRef();
     this.passwordEditButtonRef = React.createRef();
   }
+
   componentDidMount() {
     axios.get("http://localhost:4000/user/mypage").then(res => {
       console.log("res is: ", res);
@@ -49,58 +51,14 @@ class Mypage extends React.Component {
       });
     });
   }
-  onChange = async date => {
-    this.setState({ date });
-    // const stateDate = this.state.date;
-    // const sliceDate = stateDate.slice(4, 15);
-    // console.log("Full date: ", date);
-    // console.log("getMonth: ", date.getMonth());
-    let getDate =
-      date.getDate() === 1
-        ? "01"
-        : date.getDate() < 9
-        ? "0" + date.getDate() + ""
-        : date.getDate() + "";
-    let getYear = date.getFullYear() + "";
-    let getMonth =
-      date.getMonth() === 0
-        ? "01"
-        : date.getMonth() < 10
-        ? "0" + (date.getMonth() + 1) + ""
-        : date.getMonth() + 1 + "";
 
-    let chosenDate = getYear + "-" + getMonth + "-" + getDate;
-    // console.log("chosendate: ", chosenDate);
-    let data = { createdAt: chosenDate };
-    // ("2020-01-20");
-    // console.log(data);
-    axios.post("http://localhost:4000/calendar", data).then(res => {
-      console.log("calendar res is: ", res);
-      if (res.data.length > 0) {
-        alert(
-          "first todo: " +
-            res.data[0].todoitem +
-            ". second todo: " +
-            res.data[1].todoitem +
-            "."
-        );
-      } else {
-        alert("Nothing created on this date.");
-      }
-    });
-
-    // console.log("Year: ", getYear);
-    // console.log("Date: ", getDate);
-    // console.log("Month: ", getMonth);
-    // console.log("typeof year: ", typeof getYear);
-    // console.log("typeof month: ", typeof getMonth);
-    // console.log("typeof date: ", typeof getDate);
-    // console.log("typeof chosendate: ", typeof chosenDate);
-  };
+  //유저정보 수정후 엔터를 치면 자동으로 저장된다.
   handleInputValue = key => e => {
     // this.setState({ [key]: e.target.value });
     this.setState({ [key]: e.target.value });
   };
+
+  //edit 버튼을 클릭하고 유저정보를 수정한 후 save 버튼을 누르면 자동저장된다.
   handleUserEditValue = key => e => {
     e.preventDefault();
     // console.log("this.userRef is: ", this.userRef);
@@ -126,6 +84,8 @@ class Mypage extends React.Component {
         });
     }
   };
+
+  //email 정보 수정
   handleEmailEditValue = key => e => {
     console.log("this.emailRef is: ", this.emailRef);
     if (e.target.innerHTML === "Edit") {
@@ -139,7 +99,6 @@ class Mypage extends React.Component {
       e.target.innerHTML = "Edit";
       this.emailRef.current.disabled = true;
       this.emailRef.current.style.backgroundColor = "#ECF0F1";
-
       let data = { email: this.state.email };
       try {
         axios.put("http://localhost:4000/user/edit", data);
@@ -152,6 +111,8 @@ class Mypage extends React.Component {
       }
     }
   };
+
+  //비밀번호 정보 수정
   handlePasswordEditValue = key => e => {
     console.log("this.passwordRef is: ", this.passwordRef);
     if (e.target.innerHTML === "Edit") {
@@ -232,6 +193,7 @@ class Mypage extends React.Component {
       }
     }
   };
+
   render() {
     let number = (
       (this.state.completeCount / this.state.todoCount) *
@@ -239,66 +201,65 @@ class Mypage extends React.Component {
     ).toString();
     return (
       <div className="body">
-        <div className="body">
-          <div className="body">
-            <div style={{ padding: "10px", float: "right" }} className="body">
-              <Link className="loginRedirectButton" to="/login">
-                Log Out
-              </Link>
-            </div>
-            <div style={{ padding: "10px", float: "right" }} className="body">
-              <Link className="loginRedirectButton" to="/todopage">
-                Todo Page
-              </Link>
-            </div>
-          </div>
+        <div>
+          <Link className="loginRedirectButton1" to="/login">
+            로그아웃
+          </Link>
         </div>
+        <div>
+          <Link className="loginRedirectButton2" to="/todopage">
+            Todo Page
+          </Link>
+        </div>
+
         <div className="body">
-          <center className="myPageTitle">My Page</center>
+          <div className="myPageTitle">My Page</div>
         </div>
         <div className="userInfoTitle">User Info</div>
-        <div className="userStatisticsTitle">Statistics</div>
-
+        {/* ㅁ */}
         <div className="body">
-          <input
-            type="text"
-            placeholder={this.state.username}
-            id="usernameBox"
-            onChange={this.handleInputValue("username")}
-            ref={this.userRef}
-            disabled
-            onFocus={this.clearUsernameFocus()}
-            onBlur={this.onUsernameBlur()}
-            onKeyUp={this.setUserInfo("username")}
-          ></input>
-          <button
-            type="button"
-            className="userEditButton"
-            ref={this.userEditButtonRef}
-            onClick={this.handleUserEditValue()}
-          >
-            Edit
-          </button>
-
-          <input
-            type="text"
-            placeholder={this.state.email}
-            id="emailBox"
-            onChange={this.handleInputValue("email")}
-            ref={this.emailRef}
-            disabled
-            onFocus={this.clearEmailFocus()}
-            onBlur={this.onEmailBlur()}
-            onKeyUp={this.setUserInfo("email")}
-          ></input>
-          <button
-            type="button"
-            className="emailEditButton"
-            ref={this.emailEditButtonRef}
-            onClick={this.handleEmailEditValue()}
-          >
-            Edit
-          </button>
+          <div>
+            <input
+              type="text"
+              placeholder={this.state.username}
+              id="usernameBox"
+              onChange={this.handleInputValue("username")}
+              ref={this.userRef}
+              disabled
+              onFocus={this.clearUsernameFocus()}
+              onBlur={this.onUsernameBlur()}
+              onKeyUp={this.setUserInfo("username")}
+            ></input>
+            <button
+              type="button"
+              className="userEditButton"
+              ref={this.userEditButtonRef}
+              onClick={this.handleUserEditValue()}
+            >
+              Edit
+            </button>
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder={this.state.email}
+              id="emailBox"
+              onChange={this.handleInputValue("email")}
+              ref={this.emailRef}
+              disabled
+              onFocus={this.clearEmailFocus()}
+              onBlur={this.onEmailBlur()}
+              onKeyUp={this.setUserInfo("email")}
+            ></input>
+            <button
+              type="button"
+              className="emailEditButton"
+              ref={this.emailEditButtonRef}
+              onClick={this.handleEmailEditValue()}
+            >
+              Edit
+            </button>
+          </div>
 
           <input
             type="text"
@@ -320,26 +281,36 @@ class Mypage extends React.Component {
             Edit
           </button>
         </div>
-        <Calendar onChange={this.onChange} value={this.state.date} />
-        <div className="statisticsBox">
-          <DonutChart
-            className="donut"
-            data={[
-              {
-                label: "완료한 Mustodo 수",
-                value: this.state.completeCount
-              },
-              {
-                label: "미완료 Mustodo 수",
-                value: this.state.todoCount - this.state.completeCount,
-                isEmpty: true
-              }
-            ]}
-          />
 
-          <h2 align="center">작성한 Mustodo 수:</h2>
-          <h2 align="center">{this.state.todoCount}</h2>
+        {/* ㅁ */}
+        <div className="userStatisticsTitle">Statistics</div>
+
+        {/* 아래는 그래프 부분입니다. 참고하세요 */}
+        <div className="body">
+          <div align="center" className="statisticsBox">
+            <DonutChart
+              width={400}
+              height={260}
+              className="donut"
+              data={[
+                {
+                  label: "완료",
+                  value: this.state.completeCount
+                },
+                {
+                  label: "미완료",
+                  value: this.state.todoCount - this.state.completeCount,
+                  isEmpty: true
+                }
+              ]}
+            />
+
+            <h4 align="center">작성한 Mustodo 수: {this.state.todoCount}</h4>
+          </div>
         </div>
+        {/* 그래프 끝 */}
+
+        {/* <Calendar onChange={this.onChange} value={this.state.date} /> */}
       </div>
     );
   }
